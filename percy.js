c954e7253c117a960e004894520b7fbdd24533e3
KoryNunn <knunn187@gmail.com>
2014-02-12 17:40:27 +1000

    Improved to match couch API

--- percy.js
+++ percy.js
@@ -10,12 +10,12 @@ function get(key, callback) {
 }
 
 function set(key, data, callback) {
-    var model = this;
+    var percy = this;
 
     kgo
     ('bucket', this.connector)
     ('model', function(done){
-        model.validator.validate(data, done);
+        percy.validator.validate(data, done);
     })
     (['bucket', 'model'], function(bucket, model){
         bucket.set(key, model, callback);
@@ -25,11 +25,55 @@ function set(key, data, callback) {
     });
 }
 
-function Model(connector, validator){
+function add(key, data, callback){
+    var percy = this;
+
+    kgo
+    ('bucket', this.connector)
+    ('model', function(done){
+        percy.validator.validate(data, done);
+    })
+    (['bucket', 'model'], function(bucket, model){
+        bucket.add(key, model, callback);
+    }).errors({
+        bucket: callback,
+        model: callback
+    });
+}
+
+function replace(key, data, callback){
+    var percy = this;
+
+    kgo
+    ('bucket', this.connector)
+    ('model', function(done){
+        percy.validator.validate(data, done);
+    })
+    (['bucket', 'model'], function(bucket, model){
+        bucket.replace(key, model, callback);
+    }).errors({
+        bucket: callback,
+        model: callback
+    });
+}
+
+function remove(key, callback){
+    this.connector(function(error, bucket){
+        if(error){
+            return callback(error);
+        }
+        bucket.remove(key, callback);
+    });
+}
+
+function Percy(connector, validator){
     this.connector = connector;
     this.validator = validator;
 }
-Model.prototype.get = get;
-Model.prototype.set = set;
+Percy.prototype.get = get;
+Percy.prototype.set = set;
+Percy.prototype.add = add;
+Percy.prototype.replace = replace;
+Percy.prototype.remove = remove;
 
-module.exports = Model;
+module.exports = Percy;
--- tests/index.js
+++ tests/index.js
@@ -1,34 +1,162 @@
 var test = require('grape'),
     Percy = require('../');
 
-function mockConnector(callback){
-    callback(null, {
-        get: function(key, callback){
-            callback(null, {});
-        },
-        set: function(key, model, callback){
-            callback(null, model);
-        },
-    });
+function createMockConnector(){
+    var db = {};
+    return function(callback){
+        callback(null, {
+            get: function(key, callback){
+                if(!(key in db)){
+                    return callback(true);
+                }
+                callback(null, db[key]);
+            },
+            set: function(key, model, callback){
+                db[key] = model;
+                callback(null, model);
+            },
+            add: function(key, model, callback){
+                if(key in db){
+                    return callback(true);
+                }
+                db[key] = model;
+                callback(null, model);
+            },
+            remove: function(key, callback){
+                if(!(key in db)){
+                    return callback(true);
+                }
+                delete db[key];
+                callback(null);
+            },
+            replace: function(key, model, callback){
+                if(!(key in db)){
+                    return callback(true);
+                }
+                db[key] = model;
+                callback(null, model);
+            }
+        });
+    };
 }
 
-mockValidator = {
-    validate: function(data, callback){
-        callback(null, true);
-    }
-};
+function createMockValidator(){
+    return {
+        validate: function(data, callback){
+            callback(null, true);
+        }
+    };
+}
 
-test('create Model', function(t){
+test('create percy', function(t){
+
+    t.plan(1);
+
+    var percy = new Percy(createMockConnector(), createMockValidator());
+
+    t.pass('Percy was created');
+});
+
+test('set model', function(t){
+
+    t.plan(1);
+
+    var percy = new Percy(createMockConnector(), createMockValidator());
+
+    percy.set('abc', {}, function(error, model){
+        t.pass('model added');
+    });
+});
+
+test('can double set model', function(t){
 
     t.plan(2);
 
-    var percy = new Percy(mockConnector, mockValidator);
-
-    percy.get('abc', function(error, model){
-        t.pass('Got from db');
-    });
+    var percy = new Percy(createMockConnector(), createMockValidator());
 
     percy.set('abc', {}, function(error, model){
-        t.pass('set to db');
+        t.pass('model added');
+
+        percy.set('abc', {}, function(error, model){
+            t.pass('model added again');
+        });
+    });
+});
+
+test('get model', function(t){
+
+    t.plan(2);
+
+    var percy = new Percy(createMockConnector(), createMockValidator());
+
+    percy.set('abc', {}, function(error, model){
+        t.pass('model added');
+
+        percy.get('abc', function(error, model){
+            t.ok(model, 'got model');
+        });
+    });
+});
+
+test('cannot get nonexistant model', function(t){
+
+    t.plan(1);
+
+    var percy = new Percy(createMockConnector(), createMockValidator());
+
+    percy.get('abc', function(error, model){
+        t.ok(error, 'error thrown as expected');
+    });
+});
+
+test('add model', function(t){
+
+    t.plan(1);
+
+    var percy = new Percy(createMockConnector(), createMockValidator());
+
+    percy.add('abc', {}, function(error, model){
+        t.pass('model added');
+    });
+});
+
+test('cannot double-add model', function(t){
+
+    t.plan(2);
+
+    var percy = new Percy(createMockConnector(), createMockValidator());
+
+    percy.add('abc', {}, function(error, model){
+        t.pass('model added');
+    });
+
+    percy.add('abc', {}, function(error, model){
+        t.ok(error, 'error thrown as expected');
+    });
+});
+
+test('remove model', function(t){
+
+    t.plan(2);
+
+    var percy = new Percy(createMockConnector(), createMockValidator());
+
+    percy.add('abc', {}, function(error, model){
+        t.pass('model added');
+
+        percy.remove('abc', function(error, model){
+            t.equal(error, null, 'model removed');
+        });
+    });
+});
+
+test('cannot remove nonexistant', function(t){
+
+    t.plan(1);
+
+    var percy = new Percy(createMockConnector(), createMockValidator());
+
+    percy.remove('abc', function(error, model){
+        t.ok(error, 'error thrown as expected');
     });
 });
